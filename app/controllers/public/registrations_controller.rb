1caa753c557a31e4a35bf22233dfedf4c6f3e5e3
TeatimeinOrbit <108617356+TeatimeinOrbit@users.noreplay.github.com>
2022-09-12 07:09:55 +0000

    [Add] Gem 'pry-rails', 'net-smtp'; [Update] registration

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -1,8 +1,13 @@
 # frozen_string_literal: true
 
 class Public::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
+
+  # Sign_up後のリダイレクト先を指定(ユーザープロフィール編集画面へ)
+  def after_sign_up_path_for(resource_or_scope)
+    edit_public_user_path(user_id)
+  end
 
   # GET /resource/sign_up
   # def new
@@ -38,12 +43,12 @@ class Public::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute, :name])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 class Public::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
+  #before_action :configure_sign_in_params, only: [:create]
+  #before_action :user_state, only: [:create]
 
   # GET /resource/sign_in
   # def new
@@ -18,7 +19,17 @@ class Public::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+  protected
+
+  # 退会しているかを判断するメソッド
+  def user_state
+    @user = User.find_by(email: params[:user][:email])
+    return if !@user
+    # "入力したパスワードが一致しtrue" かつ "退会フラグがtrue" だった場合
+    if @user.valid_password?(params[:user][:password]) && !@user.is_deleted
+      redirect_to new_user_registration_path
+    end
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
