fb79a526fb5cc921aac21b90dd476f1aaed6a704
TeatimeinOrbit <108617356+TeatimeinOrbit@users.noreplay.github.com>
2022-09-18 09:18:53 +0000

    [Add] model: Relationship, controller: relationship, view: public/relationships/(followers, followings, _fololw_btn, _follow_list

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_16_094449) do
+ActiveRecord::Schema.define(version: 2022_09_18_072154) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -79,6 +79,13 @@ ActiveRecord::Schema.define(version: 2022_09_16_094449) do
     t.integer "time_of_day_id", default: 0, null: false
     t.string "title", null: false
     t.text "sentence", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id", null: false
+    t.integer "followed_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
